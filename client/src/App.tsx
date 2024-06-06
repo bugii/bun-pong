43bd9087cacae3e709cab708b4d9af924c34bf32
Dario Bugmann <dario.bugmann@outlook.com>
2024-06-06 13:46:23 +0200

    very cool start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,51 @@
-import type { Component } from "solid-js";
+import { createSignal, type Component } from "solid-js";
 
 const App: Component = () => {
-  return <div class="text-xl">hi</div>;
+  console.log("running main compoment");
+
+  const [roomId, setRoomId] = createSignal("");
+  const [username, setUsername] = createSignal("");
+
+  function join() {
+    console.log("joining, ", roomId());
+    const ws = new WebSocket(
+      `ws:localhost:4444?username=${username()}&roomId=${roomId()}`,
+    );
+
+    ws.onmessage = (msg) => {
+      console.log("received msg", msg.data);
+    };
+  }
+
+  return (
+    <div>
+      <div class="flex gap-4">
+        <span>enter your room id</span>
+        <input
+          onChange={(e) => {
+            setRoomId(e.target.value);
+          }}
+          class="border"
+          type="text"
+        />
+      </div>
+
+      <div class="flex gap-4">
+        <span>enter username</span>
+        <input
+          type="text"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          class="border"
+        />
+      </div>
+
+      <button onClick={join} class="disabled:text-gray-300">
+        join now
+      </button>
+    </div>
+  );
 };
 
 export default App;
--- server/index.ts
+++ server/index.ts
@@ -1,2 +1,40 @@
-console.log("Hello via Bun!");
+const server = Bun.serve<{ username: string; roomId: string }>({
+  port: 4444,
+  fetch(req, server) {
+    const url = new URL(req.url);
+
+    const success = server.upgrade(req, {
+      data: {
+        username: url.searchParams.get("username"),
+        roomId: url.searchParams.get("roomId"),
+      },
+    });
+    if (success) {
+      // Bun automatically returns a 101 Switching Protocols
+      // if the upgrade succeeds
+      return undefined;
+    }
+
+    // handle HTTP request normally
+    return new Response("Hello world!");
+  },
+  websocket: {
+    open(ws) {
+      const msg = `joined ${ws.data.username} ${ws.data.roomId}`;
+      ws.subscribe(ws.data.roomId);
+      server.publish(ws.data.roomId, msg);
+    },
+    // this is called when a message is received
+    async message(ws, message) {
+      console.log(`Received ${message}`);
+      // send back a message
+      server.publish(
+        ws.data.roomId,
+        `${ws.data.roomId} ${ws.data.username} said: ${message}`,
+      );
+    },
+  },
+});
+
+console.log(`Listening on ${server.hostname}:${server.port}`);
 
